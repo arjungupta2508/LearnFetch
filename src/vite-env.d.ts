093c8a0facc4d70fcd6d84fda4965038e7551f23
Arjun <in.arjunguptar@gmail.com>
2026-03-05 11:10:56 +0530

    init: project setup with Vite + React + TypeScript

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL : "https://ggmsssxayavftkrycbcn.supabase.co"
+readonly VITE_SUPABASE_ANON_KEY : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnbXNzc3hheWF2ZnRrcnljYmNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NzI2ODYsImV4cCI6MjA4NzI0ODY4Nn0.PWB3cLdvp0TSMnqsBHoCbMnf5PunGvQJOJMlUt7EUNUAIzaSyBO5hitKSd9xQsp6jZ0M_RgspwWTMqN8IY"
+readonly VITE_YOUTUBE_API_KEY : "AIzaSyBO5hitKSd9xQsp6jZ0M_RgspwWTMqN8IY"
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
